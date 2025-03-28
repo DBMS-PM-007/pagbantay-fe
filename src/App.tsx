76241f6b7b9368d4c790667bb093e76e1d6e05ae
Jodell <jpadlaon@up.edu.ph>
2025-03-27 23:45:19 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+console.log(API_BASE_URL);
+
+function App() {
+  return <h1>Hello, Vite + React + TypeScript!</h1>;
+}
+
+export default App;
