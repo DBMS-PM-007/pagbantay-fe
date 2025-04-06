d3065397c78c28f71da65ae0917172589e9ab877
Jodell <jpadlaon@up.edu.ph>
2025-04-05 17:19:34 -0700

    PGB-15-added-tailwind-css-to-frontend-project

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 console.log(API_BASE_URL);
 
 function App() {
-  return <h1>Hello, Vite + React + TypeScript!</h1>;
+  return (
+    <div className="flex h-screen justify-center items-center bg-gray-100">
+      <p className="text-4xl font-bold text-blue-600">
+        Tailwind is working! 🎉
+      </p>
+    </div>
+  );
 }
 
 export default App;
