0f5c9a97341f96fd1c7d34d4adbeb178f0b6d337
Justin <justinsanveloso@gmail.com>
2025-04-22 23:06:35 +0800

    feat: added RequireAuth.tsx to handle protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './pages/Home'
 import SignInPage from './pages/Sign-In'
+import RequireAuth from './components/RequireAuth';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 console.log(API_BASE_URL);
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route element={<RequireAuth />}>
+          <Route path="/" element={<Home />} />
+        </Route>
         <Route path="/sign-in" element={<SignInPage />} />
       </Routes>
     </BrowserRouter>
