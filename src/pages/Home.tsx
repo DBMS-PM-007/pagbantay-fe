b7d854a342a85686ea58e23221af031f43d22161
ThatDot <justinsanveloso@gmail.com>
2025-04-24 04:31:23 +0800

    feat: merged staging branch to feat/PGB-21-clerk-login-signup branch

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,10 @@
+import { UserButton } from "@clerk/clerk-react";
+
 export default function Home() {
   return (
-    <div>Welcome to Home!</div>
+    <div>Welcome to Home!
+      <UserButton />
+    </div>
   )
 }
 
