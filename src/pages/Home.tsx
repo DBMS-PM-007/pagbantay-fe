d35ca4efc6450a58a4b0e44a281c9e2535dd2681
ThatDot <justinsanveloso@gmail.com>
2025-04-25 22:04:03 +0800

    feat: added pagbantay logo to sign-in page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ import SignInPage from "./SignInPage";
 import SignUpPage from "./SignUpPage";
 import ToggleButton from "../components/ToggleButton";
 import Header from "../components/Header";
+import PagbantayLogo from "../assets/pagbantay_logo.png";
 
 export default function Home() {
   const [toggle, setToggle] = useState<boolean>(true);
@@ -15,7 +16,9 @@ export default function Home() {
   return (
     <div className="w-screen h-screen flex flex-col items-center">
       <Header title={toggle ? "SIGN IN" : "SIGN UP"} />
+      <UserButton />
       <div className="w-[350px] h-full mt-6 text-center justify-center flex flex-col space-y-4 bg-white text-black p-6">
+        <img src={PagbantayLogo} />
         {toggle ? <SignInPage /> : <SignUpPage />}
         <ToggleButton toggle={toggle} handleToggle={handleToggle} />
       </div>
