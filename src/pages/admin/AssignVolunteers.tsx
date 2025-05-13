fb3edb34268ab8e9b728877e121c31208fab5857
ThatDot <justinsanveloso@gmail.com>
2025-05-14 07:28:40 +0800

    feat: disabled Assign button in Assign Volunteers page if available event list is empty

--- src/pages/admin/AssignVolunteers.tsx
+++ src/pages/admin/AssignVolunteers.tsx
@@ -8,7 +8,8 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
 } from "@components/ui/dropdown-menu";
-import Loader from "@components/Loader.tsx"
+import Loader from "@components/Loader"
+import { User, Mail, ClipboardList } from "lucide-react"
 
 export default function AssignVolunteers() {
   const [users, setUsers] = useState([]);
@@ -32,17 +33,6 @@ export default function AssignVolunteers() {
 
     fetchUsers();
   }, [refetch]);
-
-  const filteredUsers = users
-    .filter((user: any) => {
-      const fullName = `${user.first_name} ${user.last_name}`.toLowerCase();
-      return fullName.includes(searchTerm.toLowerCase());
-    })
-    .sort((a: any, b: any) => {
-      const nameA = `${a.first_name} ${a.last_name}`.toLowerCase();
-      const nameB = `${b.first_name} ${b.last_name}`.toLowerCase();
-      return nameA.localeCompare(nameB);
-    });
 
   const handleAssignToStation = async (
     eventId: string,
@@ -68,6 +58,27 @@ export default function AssignVolunteers() {
     }
   };
 
+  const filteredUsers = users
+    .filter((user: any) => {
+      const fullName = `${user.first_name} ${user.last_name}`.toLowerCase();
+      return fullName.includes(searchTerm.toLowerCase());
+    })
+    .sort((a: any, b: any) => {
+      const nameA = `${a.first_name} ${a.last_name}`.toLowerCase();
+      const nameB = `${b.first_name} ${b.last_name}`.toLowerCase();
+      return nameA.localeCompare(nameB);
+    });
+
+  const availableEvents = users.filter((user: any) => {
+    user.availability.filter((a: any) =>
+      a.availability === "AVAILABLE" &&
+      a.event &&
+      !user.assignments.some(
+        (assignment: any) =>
+          assignment.event?.event_id === a.event.event_id
+      )
+    ) || [];
+  })
 
   return (
     <div className="w-screen h-screen text-center items-center flex flex-col bg-white text-black">
@@ -88,74 +99,88 @@ export default function AssignVolunteers() {
           filteredUsers.map((user: any) => (
             <div
               key={user.user_id}
-              className="w-full flex flex-col h-auto text-sm text-black overflow-hidden font-bold bg-white border border-black rounded-lg text-left
-            shadow-md hover:shadow-xl transition-all duration-300 transform hover:-translate-y-2 hover:scale-[1.01]"
+              className="w-full flex flex-col h-auto text-sm text-black overflow-hidden font-bold bg-white border border-black rounded-lg text-left shadow-md hover:shadow-xl transition-all duration-300 transform hover:-translate-y-2 hover:scale-[1.01]"
             >
-              <div className="w-full flex flex-col h-[50px] justify-center text-sm pl-[20px] pr-[20px] pt-[10px] pb-[5px] text-white font-bold bg-[maroon] border border-black">
+              <div className="w-full flex flex-row h-[50px] justify-left items-center text-sm gap-[10px] pl-[15px] pr-[15px] pt-[10px] pb-[5px] text-white font-bold bg-[maroon] border border-black">
+                <User />
                 <h2 className="font-semibold">{user.first_name} {user.last_name}</h2>
               </div>
               <div className="flex flex-col gap-3 p-5 font-semibold">
                 <div>
-                  <h2>Email: </h2>
-                  <h2 className="text-xs pl-[10px] pr-[10px]">{user.email}</h2>
+                  <div className="flex flex-row gap-[10px] items-center">
+                    <Mail />
+                    <h2>Email: </h2>
+                  </div>
+                  <h2 className="text-xs pl-[35px] pr-[10px]">{user.email}</h2>
                 </div>
                 <div>
-                  <h2>Assigned to:</h2>
+                  <div className="flex flex-row gap-[10px] items-center">
+                    <ClipboardList />
+                    <h2>Assigned to:</h2>
+                  </div>
                   {user.assignments && user.assignments.length > 0 ? (
                     user.assignments.map((assignment: any) => (
-                      <h2 key={assignment.assignment_id} className="text-xs pl-[10px] pr-[10px]">
+                      <h2 key={assignment.assignment_id} className="text-xs pl-[35px] pr-[10px]">
                         {assignment.event?.event_name || "Unnamed Event"}
                       </h2>
                     ))
                   ) : (
-                    <h2 className="text-xs pl-[10px] pr-[10px]">Not Assigned to any events</h2>
+                    <h2 className="text-xs pl-[35px] pr-[10px]">Not Assigned to any events</h2>
                   )}
                 </div>
                 <div>
                   <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                      <button className="w-auto text-sm text-black border border-black px-4 py-2 rounded-[100px]">
-                        Assign to Event
-                      </button>
-                    </DropdownMenuTrigger>
+                    {user.availability.some((a: any) =>
+                      a.availability === "AVAILABLE" &&
+                      a.event &&
+                      !user.assignments.some(
+                        (assignment: any) =>
+                          assignment.event?.event_id === a.event.event_id
+                      )
+                    ) ? (
+                      <DropdownMenuTrigger asChild>
+                        <button className="w-auto text-sm text-black border border-black px-4 py-2 rounded-[100px] hover:cursor-pointer transition-all duration-300 hover:bg-[maroon] hover:text-white">
+                          Assign to Event
+                        </button>
+                      </DropdownMenuTrigger>
+                    ) : (
+                      <div className="pointer-events-none">
+                        <DropdownMenuTrigger asChild>
+                          <button
+                            className="w-auto text-sm border border-gray-400 text-gray-400 px-4 py-2 rounded-[100px] cursor-not-allowed"
+                            disabled
+                          >
+                            Cannot be Assigned
+                          </button>
+                        </DropdownMenuTrigger>
+                      </div>
+                    )}
                     <DropdownMenuContent className="w-56">
-                      {user.availability &&
-                        user.availability.filter((a: any) =>
+                      {user.availability
+                        .filter((a: any) =>
                           a.availability === "AVAILABLE" &&
                           a.event &&
-                          !user.assignments.some((assignment: any) => assignment.event?.event_id === a.event.event_id)
-                        ).length !== 0 ? (
-                        user.availability
-                          .filter((a: any) =>
-                            a.availability === "AVAILABLE" &&
-                            a.event &&
-                            !user.assignments.some(
-                              (assignment: any) => assignment.event?.event_id === a.event.event_id
-                            )
+                          !user.assignments.some(
+                            (assignment: any) =>
+                              assignment.event?.event_id === a.event.event_id
                           )
-                          .map((a: any) => (
-                            <DropdownMenuItem
-                              key={a.availability_id}
-                              onClick={() =>
-                                handleAssignToStation(
-                                  a.event.event_id,
-                                  user.user_id,
-                                )
-                              }
-                            >
-                              {a.event.event_name} - {a.event.location}
-                            </DropdownMenuItem>
-                          ))
-                      ) : (
-                        <DropdownMenuItem disabled>No available events</DropdownMenuItem>
-                      )}
+                        )
+                        .map((a: any) => (
+                          <DropdownMenuItem
+                            key={a.availability_id}
+                            onClick={() =>
+                              handleAssignToStation(a.event.event_id, user.user_id)
+                            }
+                          >
+                            {a.event.event_name} - {a.event.location}
+                          </DropdownMenuItem>
+                        ))}
                     </DropdownMenuContent>
                   </DropdownMenu>
                 </div>
               </div>
             </div>
-          ))
-        )}
+          )))}
       </div>
     </div>
   );
